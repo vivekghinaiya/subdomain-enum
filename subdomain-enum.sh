253ef7aa94cd80380d443a533205070b486d5815
Vivek ghinaiya <56452603+vivekghinaiya@users.noreply.github.com>
2022-07-13 21:16:13 +0530

    Create subdomain-enum.sh

---
+++ subdomain-enum.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#subdomain enumration
+
+echo "subdomain collecting please wait :> " 
+
+domain=$1
+
+subdomain_enum()
+{
+#assetfinder
+assetfinder --subs-only $domain | anew -q 1.txt
+
+#sublister
+python3 ~/Tools/Sublist3r/sublist3r.py -d $domain -o 2.txt
+
+#subfinder 
+subfinder -silent -d $domain -all -o 3.txt
+
+#findomain
+findomain -t $domain -u 4.txt 
+
+#crt.sh
+curl -s https://crt.sh/\?q\=\%.$domain\&output\=json | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee 5.txt
+
+#github-subdomains
+github-subdomains -d $domain -t /root/tokens -k -o 6.txt 
+
+}
+
+subdomain_enum
+
+cat 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt| sort -u | tee uniqd.txt 
+cat uniqd.txt | httpx | tee lived.txt
+rm 1.txt 2.txt 3.txt 4.txt 5.txt 6.txt
+
+echo "FINISHED ::::::::"
